6603d4c770e5db7649d4494a9efc5ceb3c3c0458
DmitryOrlov-Orlov <58471846+DmitryOrlov-Orlov@users.noreply.github.com>
2020-07-26 16:03:03 +0500

    сверстал первую и вторую страницу
    надо доделать: удаление позиций из корщины и localStorage

---
+++ js/basket.js
@@ -0,0 +1,127 @@
+var cardsData = [{
+    id: 1,
+    name: "Golden fruit jam",
+    prise: 10.99,
+    imgPathMin: "/img/imgPathMin/1min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: true
+},
+{
+    id: 2,
+    name: "Golden fruit",
+    prise: 8.10,
+    imgPathMin: "/img/imgPathMin/2min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: true
+},
+{
+    id: 3,
+    name: "Golden",
+    prise: 8.20,
+    imgPathMin: "/img/imgPathMin/3min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: true
+}]
+
+/*---------- ЭЛЕМЕНТЫ В КОРЗИНЕ У КОТОРЫХ TRUE(начало)----------*/
+function basket() {
+    let basket = document.querySelector('.basket');
+    basket.innerHTML = '';
+    cardsData.forEach(elem => {
+        let basketToAdd = basketItem(elem);
+        basket.appendChild(basketToAdd);
+    });
+}
+
+function basketItem(product) {
+    let basketItem = document.createElement('div');
+
+    if (product.inBasket) {
+        basketItem.innerHTML = `
+        <div class="basket-item">
+            <div class="basket-img" style="background-image:url(${product.imgPathMin}"></div>
+            <div class="basket-two-text">
+                <p class="basket_text-name">${product.name}</p>
+                <p class="basket_text-p">Quantity ${product.amount} Bottle</p>
+            </div>
+            <input class="basket-quantity" placeholder="1" value='${product.amount}' type='number' data-id=${product.id}>
+    
+            <div class="basket-price">$${(product.prise * product.amount).toFixed(2)}</div>
+            <div class="basket-canceling" data-id=${product.id}></div>
+        </div>
+        `
+    }
+    return basketItem;
+}
+basket();
+/*----------ЭЛЕМЕНТЫ В КОРЗИНЕ У КОТОРЫХ TRUE(конец)----------*/
+
+
+/*----------В КАЖДОМ ЭЛЕМЕНТЕ МЕНЯТЕСЯ ЦЕНА ЕСЛИ ИЗМЕНИТЬ РУКАМИ КОЛИЧЕСТВО(начало)----------*/
+function basketQuantity() {
+    let basketQuantityAll = document.querySelectorAll('.basket-quantity');
+    basketQuantityAll.forEach(elem => {
+        elem.onchange = basketQuantityChange;
+    });
+    console.log(basketQuantityAll);
+}
+
+function basketQuantityChange(e) {
+    let basketPriceAll = document.querySelectorAll('.basket-price');
+    let id = Number(e.target.getAttribute('data-id'));
+    let res = (cardsData[id - 1].prise * e.target.value).toFixed(2);
+    cardsData[id - 1].prise = res;
+    basketPriceAll[id - 1].innerHTML = `$${res}`;
+    totalPrise();
+}
+basketQuantity();
+/*----------В КАЖДОМ ЭЛЕМЕНТЕ МЕНЯТЕСЯ ЦЕНА ЕСЛИ ИЗМЕНИТЬ РУКАМИ КОЛИЧЕСТВО(конец)----------*/
+
+
+
+/*----------ОБЩАЯ ЦЕНА (начало)----------*/
+let totalAmountPrice = document.querySelector('.totalAmount-price');
+function totalPrise() {
+    cardsData.reduce((sum, current) => {
+        totalAmountPrice.innerHTML = `$${sum + Number(current.prise)}`
+        return sum + Number(current.prise);
+    }, 0);
+}
+totalPrise();
+/*----------ОБЩАЯ ЦЕНА (конец)----------*/
+
+
+
+/*----------КОЛИЧЕСТВО ПОЗИЦИЙ В КОРЗИНЕ(начало)----------*/
+
+function itemToBasket() {
+    let sumInBasketTrue = 0;
+    cardsData.forEach(elem => {
+        if (elem.inBasket) {
+            sumInBasketTrue++;
+        }
+    });
+    document.querySelector('.card-itemAdded').innerHTML = `${sumInBasketTrue} Item added`;
+}
+
+/*----------КОЛИЧЕСТВО ПОЗИЦИЙ В КОРЗИНЕ(конец)----------*/
+
+
+
+/*----------КНОПКА УДАЛЕНИЯ ПОЗИЦИИ ИЗ КОРЗИНЫ(начало)----------*/
+const basketCancelingAll = document.querySelectorAll('.basket-canceling');
+basketCancelingAll.forEach(elem => {
+    elem.onclick = basketCancelDelete;
+})
+function basketCancelDelete(e) {
+    let id = e.target.getAttribute('data-id');
+    cardsData[id - 1].inBasket = false;
+    basket();
+    itemToBasket();
+}
+
+
+/*----------КНОПКА УДАЛЕНИЯ ПОЗИЦИИ ИЗ КОРЗИНЫ(конец)----------*/
---
+++ js/script.js
@@ -0,0 +1,184 @@
+var cardsData = [{
+    id: 1,
+    name: "Golden fruit jam",
+    prise: 10.99,
+    imgPathMin: "/img/imgPathMin/1min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: true
+},
+{
+    id: 2,
+    name: "Golden fruit jam",
+    prise: 8.10,
+    imgPathMin: "/img/imgPathMin/2min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: true
+},
+{
+    id: 3,
+    name: "Golden fruit jam",
+    prise: 8.20,
+    imgPathMin: "/img/imgPathMin/3min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: true
+},
+{
+    id: 4,
+    name: "Golden fruit jam",
+    prise: 12.00,
+    imgPathMin: "/img/imgPathMin/4min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: false
+},
+{
+    id: 5,
+    name: "Golden fruit jam",
+    prise: 8.00,
+    imgPathMin: "/img/imgPathMin/5min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: false
+},
+{
+    id: 6,
+    name: "Golden fruit jam",
+    prise: 8.00,
+    imgPathMin: "/img/imgPathMin/6min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: false
+},
+{
+    id: 7,
+    name: "Golden fruit jam",
+    prise: 8.00,
+    imgPathMin: "/img/imgPathMin/7min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: false
+},
+{
+    id: 8,
+    name: "Golden fruit jam",
+    prise: 8.00,
+    imgPathMin: "/img/imgPathMin/8min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: false
+},
+{
+    id: 9,
+    name: "Golden fruit jam",
+    prise: 8.00,
+    imgPathMin: "/img/imgPathMin/9min.svg",
+    imgPathMax: "/img/imgPathMax/1.svg",
+    amount: 1,
+    inBasket: false
+}]
+
+
+
+/* -----КАРУСЕЛЬ(начало)-------- */
+function createGallery() {
+    let createGallery = document.querySelector('.gallery');
+    createGallery.innerHTML = '';
+    cardsData.forEach(elem => {
+        let createGalleryElem = gallery(elem);
+        createGallery.appendChild(createGalleryElem);
+    });
+}
+
+function gallery(product) {
+    let gallerySpan = document.createElement('span');
+    gallerySpan.innerHTML = `
+    <div class="carousel-item">
+        <div class="carousel-img-max" style="background-image: url(${product.imgPathMax})"></div>
+        <div class=" idLike" data-id="${product.id}"></div>
+    </div>
+    `
+    return gallerySpan;
+}
+createGallery();
+/* -----КАРУСЕЛЬ(конец)-------- */
+
+
+
+/* ----------СДВИГ ВЛЕВО ГАЛЛЕРЕНИИ (начало)--------- */
+let width = 213;
+let position = 0;
+let countId = 2;
+
+document.querySelector('.next').onclick = function () {
+    let gallery = document.querySelector('.gallery');
+    if (countId >= 1 && countId <= cardsData.length - 1) {
+        position -= width;
+        gallery.style.marginLeft = position + 'px';
+        countId++;
+        navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+        firstPagePrice.innerHTML = `$${cardsData[countId - 1].prise.toFixed(2)}`;
+    }
+}
+/* ----------СДВИГ ВЛЕВО ГАЛЛЕРЕНИИ (конец)--------- */
+
+
+
+/* ----------СДВИГ ВПРАВО ГАЛЛЕРЕНИИ (начало)--------- */
+document.querySelector('.prev').onclick = function () {
+    let gallery = document.querySelector('.gallery');
+    if (countId <= cardsData.length && countId >= 2) {
+        position += width;
+        gallery.style.marginLeft = position + 'px';
+        countId--;
+        navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+        firstPagePrice.innerHTML = `$${cardsData[countId - 1].prise.toFixed(2)}`;
+    }
+}
+/* ----------СДВИГ ВПРАВО ГАЛЛЕРЕНИИ (конец)--------- */
+
+
+
+/* ----------ЛАЙК, ДОБАВИТЬ В КОРЗИНУ изменит занчение в карзине на true или на false (начало)--------- */
+document.querySelector('.header-heart').onclick = function () {
+    let idLike = document.querySelectorAll('.idLike');
+    let numCardsData = cardsData[countId - 1];
+
+    if (numCardsData.inBasket == false) {
+        numCardsData.inBasket = true;
+        idLike[countId - 1].classList.add('gallery-like');
+
+    } else {
+        numCardsData.inBasket = false;
+        idLike[countId - 1].classList.remove('gallery-like');
+    }
+}
+/* ----------ЛАЙК, ДОБАВИТЬ В КОРЗИНУ изменит занчение в карзине на true или на false (конец)--------- */
+
+
+
+/* ----------ДОБАВИТЬ\УБАВИТЬ КОЛИЧЕСТВО БУТЫЛОЧЕК(начало)--------------- */
+let navQuantity = document.querySelector('.nav-quantity');
+navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+
+document.querySelector('.minus').onclick = function () {
+    if (cardsData[countId - 1].amount >= 1) {
+        cardsData[countId - 1].amount--;
+        navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+    }
+}
+
+document.querySelector('.plus').onclick = function () {
+    cardsData[countId - 1].amount++;
+    navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+}
+/* ----------ДОБАВИТЬ УБАВИТЬ КОЛИЧЕСТВО БУТЫЛОЧЕК(начало)--------------- */
+
+
+
+/* ----------ЦЕНА У ЭЛЕМЕНТОВ МАССИВА (начало)-------------------- */
+let firstPagePrice = document.querySelector('.first-page-price');
+firstPagePrice.innerHTML = `$${cardsData[countId - 1].prise.toFixed(2)}`;
+/* ----------ЦЕНА У ЭЛЕМЕНТОВ МАССИВА (конец)-------------------- */
