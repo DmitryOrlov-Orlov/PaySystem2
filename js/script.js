d9f5fca477f18ff36aa90a6d7ec4f98698175b8c
DmitryOrlov-Orlov <58471846+DmitryOrlov-Orlov@users.noreply.github.com>
2020-07-26 22:41:43 +0500

    на первой стр устранены баги
    были логические ошибки

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var cardsData = [{
     imgPathMax: "/img/imgPathMax/1.svg",
     amount: 1,
     totalPrice: 10.99,
-    inBasket: true
+    inBasket: false
 },
 {
     id: 2,
@@ -16,7 +16,7 @@ var cardsData = [{
     imgPathMax: "/img/imgPathMax/1.svg",
     amount: 1,
     totalPrice: 8.10,
-    inBasket: true
+    inBasket: false
 },
 {
     id: 3,
@@ -26,68 +26,9 @@ var cardsData = [{
     imgPathMax: "/img/imgPathMax/1.svg",
     amount: 1,
     totalPrice: 8.20,
-    inBasket: true
-},
-{
-    id: 4,
-    name: "Golden fruit jam",
-    prise: 12.00,
-    imgPathMin: "/img/imgPathMin/4min.svg",
-    imgPathMax: "/img/imgPathMax/1.svg",
-    amount: 1,
-    totalPrice: 12.00,
     inBasket: false
 },
-{
-    id: 5,
-    name: "Golden fruit jam",
-    prise: 8.00,
-    imgPathMin: "/img/imgPathMin/5min.svg",
-    imgPathMax: "/img/imgPathMax/1.svg",
-    amount: 1,
-    totalPrice: 8.00,
-    inBasket: false
-},
-{
-    id: 6,
-    name: "Golden fruit jam",
-    prise: 8.00,
-    imgPathMin: "/img/imgPathMin/6min.svg",
-    imgPathMax: "/img/imgPathMax/1.svg",
-    amount: 1,
-    totalPrice: 8.00,
-    inBasket: false
-},
-{
-    id: 7,
-    name: "Golden fruit jam",
-    prise: 8.00,
-    imgPathMin: "/img/imgPathMin/7min.svg",
-    imgPathMax: "/img/imgPathMax/1.svg",
-    amount: 1,
-    totalPrice: 8.00,
-    inBasket: false
-},
-{
-    id: 8,
-    name: "Golden fruit jam",
-    prise: 8.00,
-    imgPathMin: "/img/imgPathMin/8min.svg",
-    imgPathMax: "/img/imgPathMax/1.svg",
-    amount: 1,
-    totalPrice: 8.00,
-    inBasket: false
-},
-{
-    id: 9,
-    name: "Golden fruit jam",
-    prise: 8.00,
-    imgPathMin: "/img/imgPathMin/9min.svg",
-    imgPathMax: "/img/imgPathMax/1.svg",
-    amount: 1,
-    totalPrice: 8.00,
-    inBasket: false
-}]
+]
 
 
 
@@ -119,17 +60,17 @@ createGallery();
 /* ----------СДВИГ ВЛЕВО ГАЛЛЕРЕНИИ (начало)--------- */
 let width = 213;
 let position = 0;
-let countId = 2;
+let countId = 1;
 
 document.querySelector('.next').onclick = function () {
     let gallery = document.querySelector('.gallery');
-    if (countId >= 1 && countId <= cardsData.length - 1) {
+    if (countId >= 0 && countId <= cardsData.length - 2) {
         position -= width;
         gallery.style.marginLeft = position + 'px';
         countId++;
-        navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+        navQuantity.innerHTML = `Quantity ${cardsData[countId].amount}`;
         let firstPagePrice = document.querySelector('.first-page-price');
-        firstPagePrice.innerHTML = `$${cardsData[countId - 1].totalPrice.toFixed(2)}`;
+        firstPagePrice.innerHTML = `$${cardsData[countId].totalPrice}`;
     }
 }
 /* ----------СДВИГ ВЛЕВО ГАЛЛЕРЕНИИ (конец)--------- */
@@ -139,13 +80,13 @@ document.querySelector('.next').onclick = function () {
 /* ----------СДВИГ ВПРАВО ГАЛЛЕРЕНИИ (начало)--------- */
 document.querySelector('.prev').onclick = function () {
     let gallery = document.querySelector('.gallery');
-    if (countId <= cardsData.length && countId >= 2) {
+    if (countId <= cardsData.length && countId >= 1) {
         position += width;
         gallery.style.marginLeft = position + 'px';
         countId--;
-        navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+        navQuantity.innerHTML = `Quantity ${cardsData[countId].amount}`;
         let firstPagePrice = document.querySelector('.first-page-price');
-        firstPagePrice.innerHTML = `$${cardsData[countId - 1].totalPrice.toFixed(2)}`;
+        firstPagePrice.innerHTML = `$${cardsData[countId].totalPrice}`;
     }
 }
 /* ----------СДВИГ ВПРАВО ГАЛЛЕРЕНИИ (конец)--------- */
@@ -155,13 +96,13 @@ document.querySelector('.prev').onclick = function () {
 /* ----------ЛАЙК, ДОБАВИТЬ В КОРЗИНУ изменит занчение в карзине на true или на false (начало)--------- */
 document.querySelector('.header-heart').onclick = function () {
     let idLike = document.querySelectorAll('.idLike');
-    let numCardsData = cardsData[countId - 1];
+    let numCardsData = cardsData[countId];
     if (numCardsData.inBasket == false) {
         numCardsData.inBasket = true;
-        idLike[countId - 1].classList.add('gallery-like');
+        idLike[countId].classList.add('gallery-like');
     } else {
         numCardsData.inBasket = false;
-        idLike[countId - 1].classList.remove('gallery-like');
+        idLike[countId].classList.remove('gallery-like');
     }
 }
 /* ----------ЛАЙК, ДОБАВИТЬ В КОРЗИНУ изменит занчение в карзине на true или на false (конец)--------- */
@@ -170,17 +111,17 @@ document.querySelector('.header-heart').onclick = function () {
 
 /* ----------ДОБАВИТЬ\УБАВИТЬ КОЛИЧЕСТВО БУТЫЛОЧЕК(начало)--------------- */
 let navQuantity = document.querySelector('.nav-quantity');
-navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+navQuantity.innerHTML = `Quantity ${cardsData[countId].amount}`;
 document.querySelector('.minus').onclick = function () {
-    if (cardsData[countId - 1].amount >= 1) {
-        cardsData[countId - 1].amount--;
-        navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+    if (cardsData[countId].amount >= 1) {
+        cardsData[countId].amount--;
+        navQuantity.innerHTML = `Quantity ${cardsData[countId].amount}`;
         totalPrise();
     }
 }
 document.querySelector('.plus').onclick = function () {
-    cardsData[countId - 1].amount++;
-    navQuantity.innerHTML = `Quantity ${cardsData[countId - 1].amount}`;
+    cardsData[countId].amount++;
+    navQuantity.innerHTML = `Quantity ${cardsData[countId].amount}`;
     totalPrise();
 
 }
@@ -190,14 +131,9 @@ document.querySelector('.plus').onclick = function () {
 
 /*-----ЦЕНА ОТДЕЛЬНО ДЛЯ КАЖДОГО ЭЛЕМЕНТА_СДВИГ(начало)-----*/
 function totalPrise() {
-    console.log(cardsData[countId - 1].id);
-
-    console.log('до ' + cardsData[countId - 1].totalPrice);
-    cardsData[countId - 1].totalPrice = (cardsData[countId - 1].amount * cardsData[countId - 1].prise).toFixed(2);
-    console.log('после ' + cardsData[countId - 1].totalPrice);
-
+    cardsData[countId].totalPrice = (cardsData[countId].amount * cardsData[countId].prise).toFixed(2);
     let firstPagePrice = document.querySelector('.first-page-price');
-    firstPagePrice.innerHTML = `$${cardsData[countId - 1].totalPrice}`;
+    firstPagePrice.innerHTML = `$${cardsData[countId].totalPrice}`;
 }
 totalPrise();
 
